0c9f758f96e00fe86989ba7687b08b4e50500ec0
loicndjoyi <loic_ndjoyi@outlook.com>
2026-03-21 23:32:24 -0400

    feat: initial setup

---
+++ tests/TodoApp.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace TodoApp.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/TodoApp.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace TodoApp.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
