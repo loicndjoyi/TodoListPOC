55d475418c4f2bbdb7e71734f3f906de449800c5
loicndjoyi <loic_ndjoyi@outlook.com>
2026-03-22 16:30:29 -0400

    feat(backend): Add EF dbContext and SQL lite config

---
+++ src/TodoApp.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Core;
+
+namespace TodoApp.Infrastructure;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public required DbSet<TodoItem> TodoItems { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<TodoItem>()
+            .Property(todo => todo.Title)
+            .IsRequired()
+            .HasMaxLength(200);
+    }
+}
